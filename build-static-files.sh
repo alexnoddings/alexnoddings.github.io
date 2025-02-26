521b1088ab623743234c61b6f91a11cee12e63e3
Alex Noddings <alexnoddings@users.noreply.github.com>
2025-02-26 18:12:27 +0000

    Move static files from deploy step into checked source control

--- build-static-files.sh
+++ build-static-files.sh
@@ -1,22 +1,3 @@
-if test -e ./src/pages/.gen/; then
-    echo "Enabling gen endpoints"
-    mv ./src/pages/.gen/ ./src/pages/~gen/
-fi
-
-echo "Running npm install"
-npm install
-
-echo "Running npm run dev in background"
-npm run dev &
-
-# Wait for Astro to start
-echo "Waiting for Astro to start..."
-until $(curl --output /dev/null --silent --head --fail http://localhost:4321/); do
-    echo "Waiting for Astro to start..."
-    sleep 2
-done
-
-echo "Astro started."
 curl http://localhost:4321/~gen/og/og.png -o ./public/meta/og.png
 
 favicons=( 
@@ -31,14 +12,3 @@ do
     echo "..."
     curl http://localhost:4321/~gen/favicon/$i -o ./public/meta/$i
 done
-
-# npm starts a new process for 'run dev', so we can't reliably capture and kill
-# the spawned process from the returned id
-# instead, once done we just kill the node process running 'astro dev'
-echo "Killing Astro process"
-pkill -f  "^node .*/node_modules/\.bin/astro dev$"
-sleep 1s
-
-# Disable gen pages for build
-echo "Disable gen endpoints"
-mv ./src/pages/~gen/ ./src/pages/.gen/
